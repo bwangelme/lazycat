b4047eb3dbb00cbddddd630f49dbe8b0d7e4262b
xuyundong <xuyundong@douban.com>
2022-04-07 10:37:28 +0800

    update

--- src/main/java/org/example/chapter2/App.java
+++ src/main/java/org/example/chapter2/App.java
@@ -4,14 +4,12 @@ public class App {
     public static void main(String[] args) {
         String kind = args[0];
         if (kind.equals("producer")) {
-            CatProducer p = new CatProducer();
             Integer partition = 1;
             String name = "douban " + partition.toString();
             Company c = Company.builder().name(name).address("jiuxianqiao").build();
-            p.send(c, partition);
+            CatProducer.send(c, partition);
         } else if (kind.equals("consumer")) {
-            CatConsumer consumer = new CatConsumer();
-            consumer.consume();
+            CatConsumer.consume();
         }
     }
 }
